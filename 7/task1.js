a8670bacc0a8cf97b79102c34a6727fd1562146b
druha95 <druha95@gmail.com>
2014-12-03 21:02:40 +0200

    +task2

--- 7/task1.js
+++ 7/task1.js
@@ -1,5 +1,5 @@
 // Your code goes here
-
+//task1
 NodeList.prototype.forEach = Array.prototype.forEach;
 
 function my_$(dom){
@@ -54,20 +54,66 @@ function my_$(dom){
       return _this;
     }
   }
+  
+  this.css = function(property, param, timing){
+    if(typeof(property) == 'object'){
+      if(param != undefined){
+        
+        setTimeout(function(){
+          for(prop in property) {
+            elements.forEach(function(item) {
+              item.style[prop] = property[prop];
+            });
+          }
+        }, param);
+        
+        return _this;
+      } else {
+        
+        for(prop in property) {
+          elements.forEach(function(item) {
+            item.style[prop] = property[prop];
+          });
+        }
+        
+        return _this;
+      }
+      
+    }
+    else {
+      if(timing != undefined){
+        setTimeout(function(){
+          elements.forEach(function(item){
+            item.style[property] = param
+          });
+        }, timing);
+        
+        return _this;
+      } else{
+        elements.forEach(function(item){
+          item.style[property] = param
+        });
+        
+        return _this;
+      }
+    }
+  }
   return this;
+  
+  
 }
 
 
 function getStyle(oElm, strCssRule){
-	var strValue = "";
-	if(document.defaultView && document.defaultView.getComputedStyle){
-		strValue = document.defaultView.getComputedStyle(oElm, "").getPropertyValue(strCssRule);
-	}
-	else if(oElm.currentStyle){
-		strCssRule = strCssRule.replace(/\-(\w)/g, function (strMatch, p1){
-			return p1.toUpperCase();
-		});
-		strValue = oElm.currentStyle[strCssRule];
-	}
-	return strValue;
+  var strValue = "";
+  if(document.defaultView && document.defaultView.getComputedStyle){
+    strValue = document.defaultView.getComputedStyle(oElm, "").getPropertyValue(strCssRule);
+  }
+  else if(oElm.currentStyle){
+    strCssRule = strCssRule.replace(/\-(\w)/g, function (strMatch, p1){
+      return p1.toUpperCase();
+    });
+    strValue = oElm.currentStyle[strCssRule];
+  }
+  return strValue;
 }
